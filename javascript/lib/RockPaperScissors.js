354125bc89598f552a4e66ea3accfc9fd456801f
ablease <alexblease@gmail.com>
2014-11-25 13:20:24 +0000

    rock beats scissors

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -2,7 +2,8 @@ function Player(name) {
   this.name = name;
 };
 
-Player.prototype.picks = function(first_argument) {
+Player.prototype.picks = function(pick) {
+  var pick = pick
 };
 
 function Game(player1, player2) {
@@ -10,6 +11,11 @@ function Game(player1, player2) {
   this.player2 = player2;
 };
 
-Game.prototype.winner = function(first_argument) {
+Game.prototype.winner = function(player1, player2) {
+  var PAIRS = { 'rock' : 'scissors',
+            'scissors' : 'paper',
+               'paper' : 'rock' };
+  if (PAIRS[this.player1.pick] == this.player2.pick) {
   return this.player1;
+  };
 };
